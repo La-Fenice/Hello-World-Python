86224add8037dba95ada0a747d3b260075d4afad
Edward Maguire <edward.maguire33@mail.dcu.ie>
2017-10-29 17:14:32 +0000

    Update port

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import flask
 app = flask.Flask(__name__)
 
@@ -6,5 +7,6 @@ def main():
     html = "Hello World!\nHow you doing?"
     print(html)
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == "__main__:
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
